ce9404a52ac931df015db49388594baea4e5f7ed
Daniel Santos <denenewton@icloud.com>
2024-04-01 04:31:53 -0300

    Update urlSlugReverse.js

--- src/service/urlSlugReverse.js
+++ src/service/urlSlugReverse.js
@@ -1,19 +1,19 @@
 const urlSlugReverse = (text) => {
   let _url = text.split('-')
-      _url = _url.join(' ').toLowerCase()
+  let url = _url.join(' ').toLowerCase()
 
-  if (_url.includes("&")) {
-    const parts = _url.split("&");
-    const url = parts[0].length > 2 ? parts[0] : parts[1];
+  if (url.includes("&")) {
+    const parts = url.split("&");
+    url = parts[0].length > 2 ? parts[0] : parts[1];
     return url;
   }
-  if (_url.includes(",")) {
-    const parts = _url.split(",");
-    const url = parts[0].length > 2 ? parts[0] : parts[1];
+  if (url.includes(",")) {
+    const parts = url.split(",");
+    url = parts[0].length > 2 ? parts[0] : parts[1];
     return url.toString();
   }
 
-  return _url
+  return url
 }
 
 export default urlSlugReverse
